44f79994cdafb00882289efbeaba27052fddc978
stallionblade <119752878+stallionblade@users.noreply.github.com>
2023-04-27 13:04:51 -0400

    Create item schema

---
+++ models/item.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const itemSchema = new Schema({
+  name: { type: String, required: true },
+  description: { type: String, required: true },
+  category: { type: String, required: true },
+  price: { type: Number, required: true },
+  brand: { type: String, required: true },
+  model: { type: String, required: true },
+  quantity: { type: Number, required: true },
+});
+
+// Create a virtual for the url
+itemSchema.virtual("url").get(function () {
+  return `/category/${this._id}`;
+});
+
+module.exports = mongoose.model("Item", itemSchema);
