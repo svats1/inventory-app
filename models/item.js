1846e4f8522297572909782a55ccfbaa685096c5
stallionblade <119752878+stallionblade@users.noreply.github.com>
2023-04-27 13:06:53 -0400

    Link item to category schema

--- models/item.js
+++ models/item.js
@@ -5,7 +5,7 @@ const Schema = mongoose.Schema;
 const itemSchema = new Schema({
   name: { type: String, required: true },
   description: { type: String, required: true },
-  category: { type: String, required: true },
+  category: { type: Schema.Types.ObjectId, ref: "Category", required: true },
   price: { type: Number, required: true },
   brand: { type: String, required: true },
   model: { type: String, required: true },
